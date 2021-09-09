e33661681380da85f5e2faaf9e7812de096cef31
ksdfg <ksdfg123@gmail.com>
2021-09-10 00:24:15 +0530

    feat: implement fiber app with middleware and registered routes

---
+++ app.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"hermes/api"
+	"hermes/config"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+func init() {
+	// Set logging format
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Read config variables
+	config.Init()
+}
+
+var (
+	port = flag.String("port", ":3000", "Port to listen on")
+	prod = flag.Bool("prod", false, "Enable prefork in Production")
+)
+
+func main() {
+	// Parse command-line flags
+	flag.Parse()
+
+	// Create fiber app
+	app := fiber.New(fiber.Config{
+		Prefork: *prod, // go run app.go -prod
+	})
+
+	// Middleware
+	app.Use(recover.New())
+	app.Use(logger.New())
+	app.Use(cors.New())
+	app.Use(compress.New())
+
+	// Register handlers
+	api.Register(app)
+
+	// Listen on port 3000
+	log.Fatal(app.Listen(*port)) // go run app.go -port=:3000
+}
